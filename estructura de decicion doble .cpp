5c21ae22a618243efde379b5eaebedc7d76d1d52
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-19 22:01:42 -0600

    Add files via upload

---
+++ estructura de decicion doble .cpp
@@ -0,0 +1,23 @@
+	#include <stdio.h>
+	using namespace std;
+int main (){
+	coust int mhr=40;
+	const double TARIFA_regular =10.0;
+	const double tarifa_extra = tarifa_regular * 1.5;
+	int ht;
+	double salario_semanal;	
+	cout<<"ingresa las horas diarias trabajadas:";
+	cin>> ht ;
+	if (horas trabajadas <= maxhotras regulares ){
+		salario_semanal =hotas_trabajadas *tarifa_regular;
+		
+	}else {
+		int horas_regulares =mhr;
+		int horas_extras =hotras:trabajadas -max_horas_regulares ;
+		salario_semanal = horas_regulares *tarifa_regular + horas_extras + tarifa_extra;
+		 
+	}
+	cout<<"el salario del empleado es$" <<salario_semanal <<endl;
+	return 0;
+	
+}
