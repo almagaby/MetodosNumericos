ae37f356b7ee7d87c4f4e8d2fcefe2119b925738
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-21 12:12:02 -0600

    Add files via upload

---
+++ if else .cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+
+int n =98;
+if (n<50)
+printf("el numero es menor que 50");
+else 
+printf ("el numero es mayor que 50 ");
+printf("fin del programa \n");
+
+return 0;
+}
