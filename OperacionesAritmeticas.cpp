a1921b2e674d2bf7ca1a6976ef2414ad759ecce3
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-22 20:19:36 -0600

    Add files via upload

---
+++ OperacionesAritmeticas.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+	printf("La suma de %d + %d es = %d\n", 3,4,3+4);
+	printf("La suma de %d + %d es = %d\n", 78787,3259,78787+3259);
+	system("Pause");
+	return 0;
+
+}
