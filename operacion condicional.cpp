045ebc89e624fd0d4d9b92c862d54d847effacbe
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-22 20:18:17 -0600

    Add files via upload

---
+++ operacion condicional.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+int d =10;
+int dC=20;
+int c =0;
+int z=0;
+z = (c) dC, d;
+printf ("el descuento es de %d \n",z);
+printf ("fin del programa \n");
+system ("pause");
+return 0;
+}
